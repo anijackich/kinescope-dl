6a70d33c11a32e584395998604aa549beb5f5fbf
anijackich <anijackich@gmail.com>
2023-08-14 15:29:29 +0300

    Edit options

--- kinescope-dl.py
+++ kinescope-dl.py
@@ -5,36 +5,27 @@ from kinescope import KinescopeVideo, KinescopeDownloader
 @click.command()
 @click.option(
     '--referer', '-r',
-    default='', required=False, help='Referer url of the site where the video is embedded'
+    required=False, help='Referer url of the site where the video is embedded'
 )
 @click.option(
     '--best-quality',
     default=False, required=False, help='Automatically select the best possible quality', is_flag=True
 )
 @click.option(
-    '--ffmpeg-bin',
-    default=None, required=False, help='Path to FFmpeg binary', type=click.Path()
-)
-@click.option(
-    '--mp4decrypt-bin',
-    default=None, required=False, help='Path to mp4decrypt binary', type=click.Path()
-)
-@click.option(
     '--temp',
     default='./temp', required=False, help='Path to directory for temporary files', type=click.Path()
 )
-@click.argument('source', type=click.STRING)
-@click.argument('output', type=click.Path())
+@click.argument('input_url', type=click.STRING)
+@click.argument('output_file', type=click.Path())
 def main(referer,
          best_quality,
-         ffmpeg_bin, mp4decrypt_bin,
-         temp, source, output):
+         temp, input_url, output_file):
     """
     kinescope-dl: Video downloader for Kinescope
     https://github.com/anijackich/kinescope-dl
 
     \b
-    <SOURCE> is url of the Kinescope video
+    <INPUT_URL> is url of the Kinescope video
     <OUTPUT> is path to the output mp4 file
     """
 
@@ -44,15 +35,15 @@ def main(referer,
           '~ https://github.com/anijackich/kinescope-dl ~\n'
           '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
 
-    is_video_id = 'https' not in source
+    is_video_id = 'https' not in input_url
 
     kinescope_video: KinescopeVideo = KinescopeVideo(
-        url=source if not is_video_id else None,
-        video_id=source if is_video_id else None,
+        url=input_url if not is_video_id else None,
+        video_id=input_url if is_video_id else None,
         referer_url=referer
     )
 
-    downloader: KinescopeDownloader = KinescopeDownloader(kinescope_video, temp, ffmpeg_bin, mp4decrypt_bin)
+    downloader: KinescopeDownloader = KinescopeDownloader(kinescope_video, temp)
 
     video_resolutions = downloader.get_resolutions()
     chosen_resolution = video_resolutions[-1] if best_quality else video_resolutions[int(input(
@@ -62,7 +53,7 @@ def main(referer,
 
     print('\n= DOWNLOADING =================')
     downloader.download(
-        output if output else f'videos/{kinescope_video.video_id.replace("-", "")}.mp4',
+        output_file if output_file else f'videos/{kinescope_video.video_id.replace("-", "")}.mp4',
         chosen_resolution
     )
     print('===============================')
